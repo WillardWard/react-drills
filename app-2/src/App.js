274f090dff5003ee54dc33d98d963c810a8d729e
Willard Ward <willardwardiii@gmail.com>
2023-04-03 19:06:43 -0500

    completed app 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      army: [
+        "Stormcast",
+        "Skaven",
+        "Beasts of Chaos",
+        "Fyreslayers",
+        "Seraphon",
+      ],
+    };
+  }
+  render() {
+    let armyList = this.state.army.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+    return <div className="App">{armyList}</div>;
+  }
 }
 
 export default App;
